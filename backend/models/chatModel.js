31d48912fe139143a5a7a194e27af01404bdd920
Tanu Mahawar <tanumahawar539@gmail.com>
2023-07-27 19:19:45 +0530

    initial commit

---
+++ backend/models/chatModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const chatSchema = mongoose.Schema(
+  {
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "UserChat" }],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: "UserChat" },
+  },
+  { timestamps: true }
+);
+
+const Chat = mongoose.model("Chat", chatSchema);
+
+module.exports = Chat;
